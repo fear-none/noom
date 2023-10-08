335e525ee751f1355470d3e83733f14cbbbabf9d
eunho <jeh4916@gmail.com>
2023-10-08 16:23:22 +0900

    2.9 Room Count part Two

--- src/server.js
+++ src/server.js
@@ -14,8 +14,6 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 function publicRooms() {
-  // const sids = wsServer.sockets.adapter.sids;
-  // const rooms = wsServer.sockets.adapter.rooms;
   const {
     sockets: {
       adapter: { sids, rooms },
@@ -40,11 +38,15 @@ wsServer.on("connection", (socket) => {
     socket.join(roomName);
     done();
     socket.to(roomName).emit("welcome", socket.nickname);
+    wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) =>
       socket.to(room).emit("bye", socket.nickname)
     );
+  });
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("new_message", (msg, room, done) => {
     socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
